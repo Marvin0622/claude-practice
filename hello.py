d9453273ce202dea05fbc7a60be37d8bf21dabb5
张骞衡 <marvinson@zhangqianhengdeiMac.local>
2026-03-27 14:41:46 +0800

    第一次提交：添加斐波那契数列程序

---
+++ hello.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for _ in range(n - 1):
+        a, b = b, a + b
+    return b
+
+for i in range(10):
+    print(fibonacci(i))
